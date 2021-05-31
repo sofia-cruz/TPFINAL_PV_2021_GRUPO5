3ce890dc90bd3a7391a046a5ba5c2af3d9ad63c4
Anabella Valdez <Anabella Valdez>
2021-05-31 11:22:54 -0300

    Creado el paquete Controller, con la clase TuristaController.
    Uso de log "BELLA" para confirmar guardado de turista, uso de
    postMapping y GetMapping, desarrollo de editarTurista, modificarTurista,
    guardarTurista, crearTurista y eliminarTurista, y uso de autowired y
    qualifier que conecta las dependencias con TuristaServiceMySQL
    
    Anabella Valdez

---
+++ src/main/java/ar/edu/unju/edm/controller/TuristaController.java
@@ -0,0 +1,73 @@
+package ar.edu.unju.edm.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ar.edu.unju.edm.model.Turista;
+import ar.edu.unju.edm.service.ITuristaService;
+
+
+@Controller
+
+public class TuristaController {
+	private static final Log BELLA = LogFactory.getLog(TuristaController.class);
+	
+	@Autowired
+	@Qualifier("impmysql")
+	ITuristaService turistaService;
+	
+	@PostMapping
+	public String guardarTurista (@ModelAttribute("unTurista") Turista nuevoTurista, Model model) {
+
+		BELLA.info("METHOD: Ingresando al metodo Guardar");
+		turistaService.guardarTurista(nuevoTurista);
+		BELLA.info("Tamaño del listado: "+ turistaService.obtenerTodosTuristas().size());
+		return null;
+	}
+	@GetMapping
+	public String crearTurista(Model model) {
+		model.addAttribute("modoEditar", false);
+		model.addAttribute("unTurista", turistaService.crearTurista());
+		model.addAttribute("turistas", turistaService.obtenerTodosTuristas());
+		return "turista";
+	}
+	
+	@PostMapping
+	public String modificarTurista(@ModelAttribute("unTurista") Turista turistaModificado, Model model) throws Exception{
+		turistaService.modificarTurista(turistaModificado);
+		return "turista";
+	}
+	
+	@GetMapping
+	public String eliminarTurista(@PathVariable(name = "idTurista")int id, Model model) throws Exception{
+		try {
+			turistaService.eliminarTurista(id);
+		} catch (Exception e) {
+			model.addAttribute("usuarioErrorMensaje", e.getMessage());
+		}
+		return null;
+	}
+	
+	@GetMapping
+	public String editarTurista(Model model, @PathVariable(name = "idTurista") int id) throws Exception{
+		try{
+			Turista encontrado = turistaService.encontrarUnTurista(id);
+			model.addAttribute("unTurista", encontrado);
+			model.addAttribute("modoEditar", true);
+		}
+		catch(Exception e){
+			model.addAttribute("usuarioErrorMensaje", e.getMessage());
+			model.addAttribute("modoEditar", false);
+		}
+		model.addAttribute("turistas", turistaService.obtenerTodosTuristas());
+		return "turista";
+	}
+}
