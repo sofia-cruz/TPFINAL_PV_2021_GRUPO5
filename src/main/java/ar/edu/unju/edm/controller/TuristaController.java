97869faca9a593cb39bf323f56696a3d6268bcb6
Anabella Valdez <Anabella Valdez>
2021-06-14 23:00:51 -0300

    Modificación para el funcionamiento de perfil
    Clases, controladores service, imp, e interfaces modificadas de turista
    
    Implementación de html para ayuda de perfil, y css

--- src/main/java/ar/edu/unju/edm/controller/TuristaController.java
+++ src/main/java/ar/edu/unju/edm/controller/TuristaController.java
@@ -26,48 +26,55 @@ public class TuristaController {
 	
 	@PostMapping("/turista/guardar")
 	public String guardarTurista (@ModelAttribute("unTurista") Turista nuevoTurista, Model model) {
-
 		BELLA.info("METHOD: Ingresando al metodo Guardar");
 		turistaService.guardarTurista(nuevoTurista);
 		BELLA.info("Tamaño del listado: "+ turistaService.obtenerTodosTuristas().size());
-		return "redirect:/turista/mostrar";
+		return "redirect:/";
 	}
+	
+	
 	@GetMapping("/turista/mostrar")
 	public String crearTurista(Model model) {
-		//model.addAttribute("modoEditar", false);
 		model.addAttribute("unTurista", turistaService.crearTurista());
 		model.addAttribute("turistas", turistaService.obtenerTodosTuristas());
 		return ("turista");
 	}
+
 	
 	@PostMapping("/turista/modificar")
 	public String modificarTurista(@ModelAttribute("unTurista") Turista turistaModificado, Model model) throws Exception{
 		turistaService.modificarTurista(turistaModificado);
-		return "redirect:/turista/mostrar";
+		BELLA.info("Turista "+ turistaModificado.getIdTurista()+ "modificado");
+		return "redirect:/turista/perfil";
 	}
 	
 	@GetMapping("/turista/eliminar/{idTurista}")
 	public String eliminarTurista(@PathVariable(name = "idTurista")int id, Model model) throws Exception{
 		try {
+			Turista existente = turistaService.encontrarUnTurista(id);
+			BELLA.info("Turista" + existente.getNombre() + "encontrado");
 			turistaService.eliminarTurista(id);
 		} catch (Exception e) {
 			model.addAttribute("usuarioErrorMensaje", e.getMessage());
 		}
-		return "redirect:/turista/mostrar";
+		return "redirect:/logout";
 	}
+	
+	
 	
 	@GetMapping("/turista/editar/{idTurista}")
 	public String editarTurista(Model model, @PathVariable(name = "idTurista") int id) throws Exception{
 		try{
 			Turista encontrado = turistaService.encontrarUnTurista(id);
 			model.addAttribute("unTurista", encontrado);
-			model.addAttribute("modoEditar", true);
+			return "editar-turista";
 		}
 		catch(Exception e){
 			model.addAttribute("usuarioErrorMensaje", e.getMessage());
-			model.addAttribute("modoEditar", false);
 		}
-		model.addAttribute("turistas", turistaService.obtenerTodosTuristas());
-		return ("turista");
+		 return "redirect:/turista/perfil";
 	}
+	
+
+	
 }
